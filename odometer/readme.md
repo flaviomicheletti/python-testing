263567db4825687b5d846ce8d385fe576b6249c5
Flávio Micheletti <flaviomicheletti@hotmail.com>
2022-12-11 00:50:00 -0300

    second commit

--- odometer/readme.md
+++
@@ -1,5 +0,0 @@
-# Readme
-
-- https://www.pythontutorial.net/python-unit-testing/python-unittest-mock/
-
-- https://stackoverflow.com/questions/61523046/unit-test-coverage-report-does-not-include-the-mocked-methods
---
+++ stage1/step1_test.py
@@ -0,0 +1,16 @@
+def uppercase_decorator(function):
+    def wrapper():
+        func = function()
+        make_uppercase = func.upper()
+        return make_uppercase
+
+    return wrapper
+
+
+def say_hi():
+    return "hello there"
+
+
+def test_step1():
+    decorate = uppercase_decorator(say_hi)
+    assert "HELLO THERE" == decorate()
