c7a3db10ce25e012526cd7c741cbcfc40fa00e62
Flávio Micheletti <flaviomicheletti@hotmail.com>
2023-02-23 19:39:45 -0300

    type hints

--- clean.sh
+++ clean.sh
@@ -4,4 +4,4 @@ sudo find . -type d -name .pytest_cache -exec rm -r {} \+
 sudo find . -type f -name '*.db' -delete
 sudo find . -type f -name '.coverage' -delete
 sudo find . -type d -name htmlcov -exec rm -r {} \+
-# sudo find . -type d -name .venv -exec rm -r {} \+     
+sudo find . -type d -name .venv -exec rm -r {} \+     
--- types-hints/my.py
+++
@@ -1,7 +0,0 @@
-def add_numbers(a: int, b: int) -> int:
-    return a + b
-
-
-print(add_numbers(10, 5))
-print(add_numbers(10.3, 5))
-print(add_numbers("Bob", "Mark"))
